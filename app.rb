c63b59216d7ca64b0f2df83f396512f7c5a4ced2
Basile Koko <basile_koko@hotmail.com>
2017-03-06 22:56:49 +0000

    routes added sucessfully

--- app.rb
+++ app.rb
@@ -4,20 +4,16 @@ require './lib/key_value.rb'
 
 class ReturnMyValue < Sinatra::Base
 
-  before do
-    @key_value_instance = KeyValue.instance
-  end
-
   get '/set' do
     @pair = request.query_string
-    @key_value_instance.store(@pair)
+    KeyValue.store(@pair)
     erb :set
   end
 
-get '/get' do
-  @key = request.query_string
-  @value = @key_value_instance.retrieve(@key)
-  erb :get
-end
+  get '/get' do
+    @key = request.query_string
+    @value = KeyValue.retrieve(@key)
+    erb :get
+  end
 
 end
